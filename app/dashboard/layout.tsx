571a665885b1b03e180de2b0a0e0670d848087fb
tazmeenilsa <tazmeenilsa@gmail.com>
2024-09-17 22:45:29 +0500

    all new changes done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import SideNav from '../ui/dashboard/sidenav'
+import { Metadata } from 'next';
 
+export const experimental_ppr = true;
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 const Layout = ({children}:{children:React.ReactNode}) => {
+    
     return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
